ed1858e1f661f7f63c26ded0d80deaf81721ce2e
mizech <michael.zech@hotmail.de>
2025-04-29 08:41:05 +0200

    Initial Commit

---
+++ PlaygroundApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaygroundApp
+//
+//  Created by Michael on 29.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaygroundApp/PlaygroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundAppApp.swift
+//  PlaygroundApp
+//
+//  Created by Michael on 29.04.25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
